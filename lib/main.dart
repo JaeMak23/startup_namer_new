67def864c41cb19f9e76d52761e5f0d66551d042
JaeMak23 <macklin.jaison@gmail.com>
2022-02-18 10:25:59 +0400

    Add icons to the list

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,74 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-final wordPair= WordPair.random();
+    return const MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+  }
+}
 
-    return MaterialApp(
-        title: 'Welcome to flutter',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcome to flutter'),
-          ),
-          body:  Center(
-            child: Text(wordPair.asPascalCase),
-          ),
-        ),
-      );
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved=<WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
+        final index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved=_saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved?Icons.favorite:Icons.favorite_border,
+        color:alreadySaved?Colors.red:null,
+        semanticLabel: alreadySaved?"Removed from saved":"saved",
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);          
+          }
+        });
+      },
+    );
   }
 }
